1b7c897d8e6ad16bea4120e794e92f7f4339f944
Andra Uritu <andra_ioana.uritu@stud.fils.upb.ro>
2023-11-06 22:40:54 +0200

    added user schema but didnt do anything with it yet

---
+++ server/src/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true
+    },
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    }
+
+});
+
+UserSchema.plugin(passportLocalMongoose); //this will add a username and password field to our schema
+
+module.exports = mongoose.model('User', UserSchema); //this will create a collection called users in our database
